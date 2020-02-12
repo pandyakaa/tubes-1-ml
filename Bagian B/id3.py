fef2f263861d2eb1914cf43979855254d36a7144
Barbariansyah <bariansyahi@gmail.com>
2020-02-12 16:25:35 +0700

    finish fi

--- Bagian B/id3.py
+++ Bagian B/id3.py
@@ -19,10 +19,9 @@ class ID3(object):
         for val in target_attributes:
             if val in target_dictionary:
                 target_dictionary[val] += 1
-                total += 1
             else:
                 target_dictionary[val] = 1
-                total += 1
+            total += 1
 
         for attr, val in target_attributes.items():
             entropy += -val/total*(math.log2(-val/total))
@@ -30,7 +29,7 @@ class ID3(object):
         return entropy
 
     @staticmethod
-    def fit(x, y, default_val = False):
+    def fit(x, labels, y, default_val = False):
         gain = []
 
         if default_val == False:
@@ -40,11 +39,35 @@ class ID3(object):
             if (all(element==target for element in y)) :
                 return Node(target, [], True)
 
-        if a.shape[1] == 0:
+        if x.shape[1] == 0:
             return Node(default_val, [], True)
 
         
         entropy = ID3.count_entropy(y)
 
-        for idx, attr in x.transpose():
+        for idx, attr in enumerate(x.transpose()):
             gain[idx] = gain(entropy,attr,y)
+        
+        idx_max = np.argmax(gain)
+        attr_values = np.unique(x.transpose[idx_max])
+
+        node = Node(labels[idx_max],  attr_values, False)
+
+        labels.pop(idx_max)
+
+        data_per_values = dict()
+        for value in attr_values:
+            value_x = np.matrix([])
+            value_y = []
+            for idx, example in enumerate(x):
+                if(example[idx_max]==value):
+                    np.vstack([value_x,example])
+                    value_y.append(y[idx])
+
+            x = np.delete(x, idx_max, axis=1)
+            data_per_values[value] = (value_x,value_y)
+            
+        for value, data in data_per_values:
+            node.set_child(value,ID3.fit(data[0],labels,data[1]))
+
+        return node
