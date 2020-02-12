351392a87bfce656fbf9417e455360beacb6efb9
I Putu Gede Wirasuta <wirasutat@gmail.com>
2020-02-12 15:39:06 +0700

    gain

--- Bagian B/id3.py
+++ Bagian B/id3.py
@@ -2,13 +2,36 @@ import numpy as np
 import math
 import Node
 
-def mode(target): 
-        return max(set(target), key = target.count) 
+
+def mode(target): return max(set(target), key=target.count)
+
 
 class ID3(object):
     def __init__(self, labels):
         self.root = None
         self.labels = labels
+
+    @staticmethod
+    def gain(entropy, attr_values, target):
+        # entropy - sum_i(frac_i * entropy_i)
+        total = 0
+        sum_next_entropy = 0
+        attr_unique_targets = dict()
+
+        for idx, attr in enumerate(attr_values):
+            if attr in attr_unique_targets:
+                attr_unique_targets[attr]['count'] += 1
+                attr_unique_targets[attr]['targets'].append(target[idx])
+            else:
+                attr_unique_target = {'count': 1, 'targets': [target[idx]]}
+                attr_unique_targets[attr] = attr_unique_target
+            total += 1
+
+        for attr, attr_unique_target in attr_unique_targets:
+            sum_next_entropy += attr_unique_target['count'] / total * \
+                ID3.count_entropy(attr_unique_target['targets'])
+
+        return entropy - sum_next_entropy
 
     @staticmethod
     def count_entropy(target_attributes):
@@ -30,21 +53,19 @@ class ID3(object):
         return entropy
 
     @staticmethod
-    def fit(x, y, default_val = False):
+    def fit(x, y, default_val=False):
         gain = []
 
         if default_val == False:
             default_val = mode(y)
-        
         for target in y:
-            if (all(element==target for element in y)) :
+            if (all(element == target for element in y)):
                 return Node(target, [], True)
 
         if a.shape[1] == 0:
             return Node(default_val, [], True)
 
-        
         entropy = ID3.count_entropy(y)
 
         for idx, attr in x.transpose():
-            gain[idx] = gain(entropy,attr,y)
+            gain[idx] = gain(entropy, attr, y)
