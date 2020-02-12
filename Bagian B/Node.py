add3e9aacb840e98466c4c6d040d5cf0e2670c62
I Putu Gede Wirasuta <wirasutat@gmail.com>
2020-02-12 13:29:03 +0700

    fix node type checking

--- Bagian B/Node.py
+++ Bagian B/Node.py
@@ -1,5 +1,5 @@
-class Node(Object):
-    def __init__(self, attr_name: String, attr_values: List, is_leaf: Boolean):
+class Node(object):
+    def __init__(self, attr_name: str, attr_values: list, is_leaf: bool):
         self.attr_name = attr_name
         self.is_leaf = is_leaf
 
@@ -10,14 +10,24 @@ class Node(Object):
             for attr_value in attr_values:
                 self.children[attr_value] = None
 
-    def set_child(self, value: String, child: Node):
+    def __str__(self):
+        attr_values_str = '' if self.is_leaf else ' '.join(
+            self.children.keys())
+        return self.attr_name if self.is_leaf else self.attr_name + ': ' + attr_values_str
+
+    def set_child(self, value: str, child: object):
         if value in self.children:
             self.children[value] = child
         else:
             raise KeyError()
 
-    def get_child(self, value: String):
+    def get_child(self, value: str):
         if value in self.children:
             return self.children[value]
         else:
             raise KeyError()
+
+
+if __name__ == "__main__":
+    node = Node('windy', ['a', 'b'], False)
+    print(node)
