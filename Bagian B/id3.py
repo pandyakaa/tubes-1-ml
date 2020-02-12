c7274361669a38edb7c748262962f71e41a85c05
Barbariansyah <bariansyahi@gmail.com>
2020-02-12 13:31:03 +0700

    first fit init

--- Bagian B/id3.py
+++ Bagian B/id3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import Node
 
 def count_entropy(target_attributes):
     target_dictionary = dict()
@@ -11,10 +12,20 @@ def count_entropy(target_attributes):
             target_dictionary[val] += 1
             total += 1
         else:
-            target_dictionary[val] = 0
+            target_dictionary[val] = 1
             total += 1
 
     for attr, val in target_attributes.items():
         entropy += -val/total*(math.log2(-val/total))
 
     return entropy
+
+def mode(target): 
+    return max(set(target), key = target.count) 
+
+def fit(x, y, defaultVal = False):
+    if defaultVal == False:
+        defaultVal = mode(y)
+    for target in y:
+        if (all(element==target for element in y)) :
+            return Node(target, [], True)
