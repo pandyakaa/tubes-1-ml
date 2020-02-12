4afc74ff8b499a20489bb10ea1ef2e38923f384a
Barbariansyah <bariansyahi@gmail.com>
2020-02-12 13:16:50 +0700

    entropy

--- Bagian B/id3.py
+++ Bagian B/id3.py
@@ -1,2 +1,20 @@
-class classname(object):
-    pass
+import numpy as np
+import math
+
+def count_entropy(target_attributes):
+    target_dictionary = dict()
+    total = 0
+    entropy = 0
+
+    for val in target_attributes:
+        if val in target_dictionary:
+            target_dictionary[val] += 1
+            total += 1
+        else:
+            target_dictionary[val] = 0
+            total += 1
+
+    for attr, val in target_attributes.items():
+        entropy += -val/total*(math.log2(-val/total))
+
+    return entropy
