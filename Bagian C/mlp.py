6bcb855233ebd0d3e4d1df1c07fd2d5483de208f
pandyakaa <pandyaka.aptanagi@gmail.com>
2020-02-27 14:28:46 +0700

    merge with print score

--- Bagian C/mlp.py
+++ Bagian C/mlp.py
@@ -4,13 +4,14 @@ import random
 
 class MyMlp(object):
 
-    [np.array([[1,2],[3,4]])]
+    [np.array([[1, 2], [3, 4]])]
 
     def __init__(self, input_layer, hidden_layer, output_layer):
         self.input_layer = input_layer
         self.hidden_layer = hidden_layer
         self.output_layer = output_layer
-        self.layers = np.append(np.append(input_layer,hidden_layer), output_layer)
+        self.layers = np.append(
+            np.append(input_layer, hidden_layer), output_layer)
         self.weights = []
         self.n_layer = 2 + len(hidden_layer)
 
@@ -28,45 +29,47 @@ class MyMlp(object):
     def fit(self, x_train: np.array, y_train: np.array, treshold: float, mini_batch_size=10, epochs=500):
         n = len(x_train)
         for epoch in range(epochs):
-            random.shuffle(x_train)
             mini_batches = [x_train[i:i+mini_batch_size]
                             for i in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_batch(mini_batch)
+                self.update_batch(mini_batch.T)
 
     def update_batch(self, mini_batch):
-        # Update weight per batch
-        pass
+        # Do feed forward
+        # Return from feed forward passed to back_propagation
+        # Target diubah dari satu kolom, jadi n kolom dengan nilai masing-masing (contoh : [[1,0,0], [0,1,0]])
+        # Update self.weights with result from back_propagation
+        print(mini_batch)
 
     def predict(self, x_test: np.array) -> np.array:
         pass
+
     def score(self, x_test: np.array, y_test: np.array) -> float:
         # Output : akurasi dari model
         correct = 0
         y_predict = self.predict(x_test)
         for out, target in zip(y_predict, y_test):
-            if(np.array_equal(out,target)):
-                correct+=1
+            if(np.array_equal(out, target)):
+                correct += 1
         return correct/x_test.shape[0]
 
     def __str__(self):
-        for l in range(0,len(self.weights)):
+        for l in range(0, len(self.weights)):
             weight = self.weights[l]
             layer = 'Layer-'+str(l)+'('
-            for i in range(0,weight.shape[0]):
+            for i in range(0, weight.shape[0]):
                 weightRow = weight[i]
-                for j in range(0,len(weightRow)):
+                for j in range(0, len(weightRow)):
                     w = layer+str(j)+'-'+str(i)+') : '+str(weightRow[j])
-                    print(w, end='  ')    
+                    print(w, end='  ')
             print('\n')
-
 
 
 if __name__ == "__main__":
     input_layer = [0, 1]
     hidden_layer = [2]
     output_layer = [0, 1]
-    x_train = np.array([1, 2, 3, 4])
-    y_train = np.array([1, 2, 3, 4])
+    x_train = np.array([[1, 2], [3, 4]])
+    y_train = np.array([[1], [2]])
     mlp = MyMlp(input_layer, hidden_layer, output_layer)
     mlp.fit(x_train, y_train, 0.2)
